4acf15302c655456e5cb7eec5cc3c1cb7a27248d
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-02-21 10:29:36 -0500

    lab done with properties objects deconstructed

---
+++ src/components/Tweet.js
@@ -0,0 +1,38 @@
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+
+function Tweet({ tweet }) {
+
+  let { user, timestamp, message } = tweet
+
+  let { name, image, handle } = user
+
+  return (
+    <div className="tweet">
+
+    <ProfileImage image={image} />
+
+      <div className="body">
+        <div className="top">
+
+          <User name={name} handle={handle} />
+
+          <Timestamp timestamp={timestamp} />
+          
+        </div>
+
+        <Message message={message}/>
+
+        <Actions />
+
+      </div>
+
+      <i className="fas fa-ellipsis-h"></i>
+    </div>
+  );
+}
+
+export default Tweet;
